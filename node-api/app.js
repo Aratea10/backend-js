ab26e4e3ad7cd8afb7c11363e9c4285bc2f959b8
Sara <saragallegomendez10@gmail.com>
2025-10-29 18:14:59 +0100

    Connect to mongo from app.js

--- node-api/app.js
+++ node-api/app.js
@@ -1,60 +1,72 @@
-import express from 'express';
-import morgan from 'morgan';
-import { renderFile } from 'ejs';
+import express from "express";
+import morgan from "morgan";
+import { renderFile } from "ejs";
+
+// Database
+import { dbClient } from "./lib/connectMongo.js";
 
 // Middlewares
-import { filterAdminPath, filterFirefox } from './lib/middlewares/authMiddleware.js';
-import { serverErrorHandler, notFoundErrorHandler } from './lib/middlewares/errorMiddleware.js';
+import {
+  filterAdminPath,
+  filterFirefox,
+} from "./lib/middlewares/authMiddleware.js";
+import {
+  serverErrorHandler,
+  notFoundErrorHandler,
+} from "./lib/middlewares/errorMiddleware.js";
 
 // Routes
-import { router as webRouter } from './routes/webRoutes.js';
-import { router as apiRouter } from './routes/apiRoutes.js';
+import { router as webRouter } from "./routes/webRoutes.js";
+import { router as apiRouter } from "./routes/apiRoutes.js";
 
 const app = express();
 
+// Top level await disponible desde ES2022 en modulos.
+const client = await dbClient.connect();
+console.log(`Connected to MongoDB: ${client.db().databaseName}`);
+
 app.use(express.json());
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.engine('html', renderFile); // Con esto podemos escrivir nuestros templates con extensión .html
-app.set('views', './views');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.engine("html", renderFile); // Con esto podemos escrivir nuestros templates con extensión .html
+app.set("views", "./views");
 
 // 3rd Party Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 /**
  * Middlewares
  ********/
 
 app.use((req, res, next) => {
-    // Si algún middlewere no llama a next() la petición se queda congelada.
-    // console.log("Soy un middlewere");
-    // res.send("Hola mundo");
-    next();
+  // Si algún middlewere no llama a next() la petición se queda congelada.
+  // console.log("Soy un middlewere");
+  // res.send("Hola mundo");
+  next();
 });
 
 // Setting Environment
 app.use((req, res, next) => {
-    res.locals.env = process.env.NODE_ENV;
-    next();
+  res.locals.env = process.env.NODE_ENV;
+  next();
 });
 
 app.use((req, res, next) => {
-    // res.status(403).send("Access forbidden");
-    next();
+  // res.status(403).send("Access forbidden");
+  next();
 });
 
-
 app.use((req, res, next) => {
-    // console.log(req);
-    // console.log({
-    //     headers: req.headers,
-    //     host: req.host,
-    //     url: req.url,
-    //     method: req.method,
-    //     ip: req.ip,
-    // })
-    next();
+  // console.log(req);
+  // console.log({
+  //     headers: req.headers,
+  //     host: req.host,
+  //     url: req.url,
+  //     method: req.method,
+  //     ip: req.ip,
+  // })
+  next();
 });
 
 /**
@@ -63,14 +75,11 @@ app.use((req, res, next) => {
 app.use(filterAdminPath);
 app.use(filterFirefox);
 
-
-
 /**
  * Routes
  ********/
-app.use('/', webRouter);
-app.use('/api', apiRouter);
-
+app.use("/", webRouter);
+app.use("/api", apiRouter);
 
 /**
  * Error Handlers
@@ -83,6 +92,5 @@ app.use(serverErrorHandler);
 // 404 Error Handler
 // Este debe ser el último middleware
 app.use(notFoundErrorHandler);
-
 
 export default app;
