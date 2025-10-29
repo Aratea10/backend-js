ce8ea6dbde20e666e96d507e11421b3cee56d86e
Sara <saragallegomendez10@gmail.com>
2025-10-29 17:50:03 +0100

    TODO: get all paginated done

--- node-api/controllers/todoController.js
+++ node-api/controllers/todoController.js
@@ -2,6 +2,14 @@ import { todos } from "../data/Todos.js";
 
 export const todoController = {
   getAll: (req, res) => {
-    res.status(200).json(todos);
+    let retTodos = todos;
+
+    if (req.query.limit || req.query.skip) {
+      const limit = parseInt(req.query.limit);
+      const skip = parseInt(req.query.skip) || 0;
+      retTodos = retTodos.slice(skip, isNaN(limit) ? undefined : limit + skip);
+    }
+
+    res.status(200).json(retTodos);
   },
 };
