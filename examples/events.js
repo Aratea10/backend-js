a6c7abf082e8626c61551b3114a48597267b5041
Sara <saragallegomendez10@gmail.com>
2025-12-30 18:28:48 +0100

    feat: implement interval-based random event emitter with Mayoria event

--- examples/events.js
+++ examples/events.js
@@ -14,3 +14,20 @@ eventEmitter.emit('customEvent');
 // TODO: Crea una función que cada segundo genere un numero random (0,1), si es mayor que 0,5 se debe lanzar un evento llamado Mayoria
 // TODO: Crea un listener para ese evento Mayoria
 // TODO: el bucle debe ejecutarse 10 veces
+
+let n = 0;
+const interval = setInterval(() => {
+    const random = Math.random();
+    if (random > 0.5) {
+        n++;
+        eventEmitter.emit('Mayoria');
+    }
+    if (n === 2) {
+        // clearInterval(interval);
+        process.exit(0);
+    };
+}, 1000);
+
+eventEmitter.on('Mayoria', () => {
+    console.log('Es mayoría!!');
+});
