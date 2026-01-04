d196d8a8c8afd5d9dbb2088c6af099c2f7c2b8b3
Sara <saragallegomendez10@gmail.com>
2026-01-04 13:51:10 +0100

    feat: initialize Express application, integrate EJS and MongoDB, and add a basic login form with controller and routes

---
+++ node-api/controllers/loginController.js
@@ -0,0 +1,12 @@
+export const loginController = {
+
+    index: (req, res, next) => {
+        res.render('login.html');
+    },
+
+    postLogin: (req, res, next) => {
+        console.log(req.body);
+        // TODO: Busca si el usuario existe y el password es correcto.
+        res.redirect('/');
+    },
+};
